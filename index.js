d55ea896ebc513c07d7628e6272846c0ae0bc755
Vishnu <vishu19042005@gmail.com>
2025-09-17 19:12:24 +0530

    added local storage and some changes in html

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 let myleads = []
 const inputel = document.querySelector("#input-el")
 const inputbtn = document.querySelector("#input-btn")
+const dltbtn = document.querySelector("#dlt-btn")
 const ulel = document.querySelector("#ulel")
+
+let leadsfromlocalstorage=JSON.parse(localStorage.getItem("myleads"))
+
+if (leadsfromlocalstorage){
+    myleads=leadsfromlocalstorage
+    renderleads()
+}
 
 inputbtn.addEventListener("click", function () {
     myleads.push(inputel.value)
     inputel.value = "" // optional: clear input after adding
+    localStorage.setItem("myleads",JSON.stringify(myleads))
     renderleads()
 })
+
+dltbtn.addEventListener("dblclick", function () {
+    localStorage.clear()
+    myleads=[]
+    renderleads()
+    
+})
+
 
 function renderleads() {
     listitems=""
